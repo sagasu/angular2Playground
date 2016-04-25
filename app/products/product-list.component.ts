7ac38b51a8c7063f558b0110e9c84e0e3c6ef40f
sagasu <sagasu@gmail.com>
2016-04-25 16:41:36 +0100

    rearanging product-list component

---
+++ app/products/product-list.component.ts
@@ -0,0 +1,8 @@
+import {Component} from "angular2/core";
+@Component({
+    selector: 'nk-products',
+    templateUrl: 'app/products/product-list.component.html'
+})
+export class ProductListComponent {
+
+}
