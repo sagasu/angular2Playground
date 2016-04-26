897861ee104057ba97d76a246e053556262fb0e8
sagasu <sagasu@gmail.com>
2016-04-26 20:44:19 +0100

    Implementing a simple cutom pipe for filtering products

---
+++ app/products/product-filter.pipe.ts
@@ -0,0 +1,18 @@
+import {PipeTransform, Pipe} from "angular2/core";
+import {IProduct} from "./product";
+
+@Pipe({
+    name: 'productFilter'
+})
+
+export class ProductFilterPipe implements PipeTransform{
+
+    transform(value:IProduct, args:any[]): IProduct[] {
+        let filter :string = args[0] ? args[0].toLocaleLowerCase() : null;
+        // filter is a method from array object arr.filter
+        // https://developer.mozilla.org/en/docs/Web/JavaScript/Reference/Global_Objects/Array/filter
+        return filter ? value.filter((product: IProduct) =>
+            product.productName.toLocaleLowerCase().indexOf(filter) != -1) : value;
+    }
+
+}
--- app/products/product-list.component.ts
+++ app/products/product-list.component.ts
@@ -1,10 +1,12 @@
 import {Component, OnInit} from "angular2/core";
 import {IProduct} from "./product";
+import {ProductFilterPipe} from "./product-filter.pipe";
 
 @Component({
     selector: 'mk-products',
     templateUrl: 'app/products/product-list.component.html',
-    styleUrls: ['app/products/product-list.component.css']
+    styleUrls: ['app/products/product-list.component.css'],
+    pipes: [ProductFilterPipe]
 })
 export class ProductListComponent implements OnInit {
     ngOnInit():any {
