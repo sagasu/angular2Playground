d8270b3cb59719aafc02d1272726b0a4c4daba8d
sagasu <sagasu@gmail.com>
2016-04-30 20:36:15 +0100

    injecting products from product service

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,5 +1,6 @@
 import {Component} from "angular2/core";
 import {ProductListComponent} from "./products/product-list.component";
+import {ProductService} from "./products/product.service";
 
 @Component({
     selector: 'mk-app',
@@ -9,7 +10,8 @@ import {ProductListComponent} from "./products/product-list.component";
         <mk-products></mk-products>
     </div>
     `,
-    directives: [ProductListComponent]
+    directives: [ProductListComponent],
+    providers: [ProductService]
 })
 export class AppComponent{
     pageTitle: string = 'simple angular 2 app'
